628408596d355ea713eb02792a424711d5605626
Alisa <112906693+AlisEtinger@users.noreply.github.com>
2022-09-14 14:00:41 +0300

    Add files via upload

---
+++ Мини ИИ.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+while(True):
+    question = input()
+    answer = question.replace('？', 'что')
+    answer = answer.replace('？', '！')
+    print(answer)
+
+
+# In[ ]:
+
+
+
+
